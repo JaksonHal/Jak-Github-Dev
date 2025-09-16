857ada624706e62fa4e9e7d521b406a133c64120
Halford, Jakson N <jakson.halford@okstate.edu>
2025-09-16 15:55:11 -0500

    Name - Jakson Halford

--- JH Basic App GithubDev/Class1.cs
+++ JH Basic App GithubDev/Class1.cs
@@ -7,7 +7,7 @@ namespace JH_Basic_App_GithubDev
         public string Name { get; }
         public Class1()
         {
-            Name = "[Firstname Lastname] - The Code Master"; // Insert your name and moniker
+            Name = "Jakson Halford - The Code Master"; // Insert your name and moniker
         }
     }
 }
